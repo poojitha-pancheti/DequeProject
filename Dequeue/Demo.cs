27d9dedce09b71588394e6b43f0dc06248569e55
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-07 16:22:33 +0530

    Add project files.

---
+++ Dequeue/Demo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Dequeue
+{
+    class DequeueA
+    {
+        static void Main(string[] args)
+        {
+            int choice, x;
+            DequeueA1 dq = new DequeueA1(8);
+            while(true)
+            {
+                Console.WriteLine("1.Insert at the front end");
+                Console.WriteLine("2.Insert at the rear end");
+                Console.WriteLine("3.Delete from front end");
+                Console.WriteLine("4.Delete from rear end");
+                Console.WriteLine("5.Display all elements of dequeue");
+                Console.WriteLine("6.Quit");
+                Console.WriteLine("Enter your choice:");
+                choice = Convert.ToInt32(Console.ReadLine());
+                if (choice == 6)
+                    break;
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter the element to be inserted at front");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        dq.InsertFront(x);
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the element to be inserted at end");
+                        x = Convert.ToInt32(Console.ReadLine());
+                        dq.InsertRear(x);
+                        break;
+                    case 3:
+                        Console.WriteLine("Element deleted from front end:" + dq.DeleteFront());
+                        break;
+                    case 4:
+                        Console.WriteLine("Element deleted from rear end:" + dq.DeleteRear());
+                        break;
+                    case 5:
+                        dq.Display();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Dequeue/DequeueA.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Dequeue
+{
+    class DequeueA1
+    {
+        private int[] queueArray;
+        private int front;
+        private int rear;
+        public DequeueA1()
+        {
+            queueArray = new int[10];
+            front = -1;
+            rear = -1;
+        }
+        public DequeueA1(int maxSize)
+        {
+            queueArray = new int[maxSize];
+            front = -1;
+            rear = -1;
+        }
+        public void InsertFront(int x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue overflow");
+                return;
+            }
+            if (front == -1)
+            {
+                front = 0;
+                rear = 0;
+            }
+            else if (front == 0)
+                front = queueArray.Length - 1;
+            else
+                front = front - 1;
+            queueArray[front] = x;
+        }
+        public void InsertRear(int x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Queue overflow");
+                return;
+            }
+            if (front == -1)
+                front = 0;
+            if (rear == queueArray.Length - 1)
+                rear = 0;
+            else
+                rear = rear + 1;
+            queueArray[rear] = x;
+        }
+        public int DeleteFront()
+        {
+            int x;
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Queue underflow");
+            x = queueArray[front];
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else if (front == queueArray.Length - 1)
+                front = 0;
+            else
+                front = front + 1;
+            return x;
+        }
+        public int DeleteRear()
+        {
+            int x;
+            if (IsEmpty())
+                throw new System.InvalidOperationException("Queue underflow");
+            x = queueArray[rear];
+            if (front == rear)
+            {
+                front = -1;
+                rear = -1;
+            }
+            else if (front ==rear)
+                rear = queueArray.Length - 1;
+            else
+                front = front - 1;
+            return x;
+        }
+        public bool IsFull()
+        {
+            return ((front == 0 && rear == queueArray.Length - 1) || (rear == rear + 1));
+        }
+        public bool IsEmpty()
+        {
+            return (front == -1);
+        }
+        public void Display()
+        {
+            int i;
+
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Console.WriteLine("Queue is :");
+            i = front;
+            if(front <= rear)
+            {
+                while (i <= rear)
+                    Console.Write(queueArray[i++] + " ");
+            }
+            else
+            {
+                while (i <= queueArray.Length - 1)
+                    Console.WriteLine(queueArray[i++] + " ");
+                i = 0;
+                while (i <= rear)
+                    Console.WriteLine(queueArray[i++] + " ");
+
+            }
+            Console.WriteLine(" ");
+        }
+
+    }
+}
